f432592250c9229300bde4beb239a85615edcd0c
nikolay-egorov <deathadder98@gmail.com>
2019-03-24 20:08:04 +0300

    Initial commit

---
+++ js/quote.js
@@ -0,0 +1,6 @@
+
+var canvas = document.getElementById("title_canvas"), 
+    context = canvas.getContext("2d");
+context.font = "30px Verdana";
+context.strokeStyle = "black";
+context.strokeText("I'm canvas!", 20, 50);
