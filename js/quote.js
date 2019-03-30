3ef9561efb37f53183d5b762dbd48b025f04d543
nikolay-egorov <deathadder98@gmail.com>
2019-03-30 18:48:27 +0300

    collage is done

--- js/quote.js
+++ js/quote.js
@@ -15,10 +15,10 @@
 
 var endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 var canvas = document.getElementById("title_canvas");
-canvas.setAttribute('width', 1600);
-canvas.setAttribute('height', 1200);
-var w_sample = canvas.width / 3 >> 0;
-var h_sample = canvas.height / 2 >> 0;
+canvas.setAttribute('width', 700);
+canvas.setAttribute('height', 700);
+var w_sample = canvas.width / 2 >> 0;
+var h_sample = canvas.height / 3 >> 0;
 var globalWidth = canvas.width;
 
 
@@ -42,21 +42,22 @@ function displayQuote(quote) {
 }
 
 
-function drawImageScaled(image, x, y, fullwidth) {
+function drawImageScaled(  x, y, fullwidth) {
     // use context.height and context.width to draw a fullscreen
-    // var w_sample = w_sample;
-    // if (fullwidth){
-    //     w_sample = globalWidth;
-    // }
+    var vRatio = h_sample / this.height;
+    if (fullwidth){
+        this.w_sample = globalWidth;
+        vRatio = globalWidth * w_sample;
+    }
+    else this.w_sample=w_sample;
     var context = document.getElementById("title_canvas").getContext("2d");
-    var hRatio = w_sample / image.width;
-    var vRatio = h_sample / image.height;
+    var hRatio = this.w_sample / this.width;
     var ratio = Math.min(hRatio, vRatio);
-    var centerShift_x = (w_sample - image.width * ratio) / 2;
-    var centerShift_y = (h_sample - image.height * ratio) / 2;
-    context.clearRect(x, y, image.width * ratio, image.height * ratio);
-    context.drawImage(image, x, y,
-         image.width * ratio, image.height * ratio);
+    // var centerShift_x = (this.w_sample - this.width * ratio) / 2;
+    // var centerShift_y = (h_sample - this.height * ratio) / 2;
+    context.clearRect(x, y, this.width * ratio, this.height * ratio);
+    context.drawImage(this, x, y,
+        this.w_sample, h_sample);
 }
 
 
@@ -77,44 +78,31 @@ function drawImageScaled(image, x, y, fullwidth) {
 */
 function buildCollage() {
 
-    var images = [];
-    var numImages = 6;
-    var tempWidth = 0;
-    var src = "https://source.unsplash.com/random?sig=";
+    var numImages = 5;
+    var src = "https://source.unsplash.com/400x300?sig=";
     var x = 0, y = 0;
     for (var i = 0; i < numImages; i++) {
-        if (tempWidth < globalWidth - 1) {
-            fetch(src + Math.random())
-                .then(function (response) {
-                    return response.url;
-                })
-                .then(function (data) {
-                    // var base64img= URL.createObjectURL(data);
-                    drawImgFromUnsplash(context, images[i], data, x, y, 0)
-                });
-            tempWidth += w_sample;
+        if (x < globalWidth - 4) {
+            drawImgFromUnsplash(src+ Math.random(), x, y, 0);
             x += w_sample;
         } else {
-            tempWidth = 0;
             x = 0;
             y += h_sample;
-            if (i === numImages - 1) {
-                fetch(src + Math.random()
-                ).then(function (response) {
-                    return response.blob();
-                }
-                    .then(function (data) {
-                        var base64img = URL.createObjectURL(data);
-                        drawImgFromUnsplash(context, images[i], base64img, x, y, 1)
-                    }));
-
+            if (i === numImages -1 ) {
+                // fetch(src + Math.random()
+                // ).then(function (response) {
+                //     return response.blob();
+                // }
+                //     .then(function (data) {
+                //         var base64img = URL.createObjectURL(data);
+                //         drawImgFromUnsplash(context, images[i], base64img, x, y, 1)
+                //     }));
+                drawImgFromUnsplash(src+ Math.random(), x, y, 1);
                 // drawImgFromUnsplash(context,images[i],src + Math.random(),x,y,1);
                 break;
             }
-            fetch(src + Math.random()
-            ).then(function (response) {
-                drawImgFromUnsplash(context, images[i], response.url, x, y, 0)
-            });
+            drawImgFromUnsplash(src+ Math.random(), x, y, 0);
+            x += w_sample;
             // drawImgFromUnsplash(context,images[i],src + Math.random(),x,y,0);
         }
     }
@@ -167,12 +155,7 @@ function scaleWidth(img) {
 
 function drawImgFromUnsplash(src, x, y, fullwidth) {
     var image = new Image();
-    // image.addEventListener("load", function() {
-    //     var context = canvas.getContext("2d");
-    //     drawImageScaled(context,this,x,y,fullwidth);
-    // }, false);
-
-    image.onload = drawImageScaled.bind(null, image, x, y, fullwidth);
+    image.onload = drawImageScaled.bind( image, x, y, fullwidth);
     image.src = src;
 }
 
@@ -210,7 +193,7 @@ function getLines(context, text, x, y, maxWidth, lineHeight) {
             metrics = context.measureText(test);
         }
         if (text[i] != test) {
-            text.splice(i + 1, 0, text[i].substr(test.length))
+            text.splice(i + 1, 0, text[i].substr(test.length));
             text[i] = test;
         }
 
@@ -227,14 +210,17 @@ function getLines(context, text, x, y, maxWidth, lineHeight) {
         }
     }
 }
-f();
 
+buildCollage();
+// var x = 0, y = 0;
+// drawImgFromUnsplash("https://source.unsplash.com/400x400?sig=" + Math.random(), x, y, 0);
+// drawImgFromUnsplash("https://source.unsplash.com/400x400?sig=" + Math.random(), x+w_sample, y, 0);
+// x=0;
+// y+=h_sample;
+// drawImgFromUnsplash("https://source.unsplash.com/400x400?sig=" + Math.random(), x, y, 0);
+// drawImgFromUnsplash("https://source.unsplash.com/400x400?sig=" + Math.random(), x, y+h_sample, 1);
 // randomUnsplash();
-function f() {
-    var x = 0, y = 0;
-    drawImgFromUnsplash("https://source.unsplash.com/400x300?sig=" + Math.random(), x, y, 0);
-    drawImgFromUnsplash("https://source.unsplash.com/400x300?sig=" + Math.random(), x+w_sample, y, 0);
-}
+
 
 // text = 'Antidisestablishmentarianism dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. Praesent id metus massa, ut antidisestablishmentarianism odio. Proin quis tortor orci. Etiam at risus et  justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum  laoreet. Nunc eu antidisestablishmentarianism orci. Quisque eget odio ac lectus vestibulum faucibus  eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus  tortor. Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate.';
 // var quoteText = document.getElementById('title_canvas');
