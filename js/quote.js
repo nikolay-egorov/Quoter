8717561f43ed3dd8adc4c99e5511019791be568a
nikolay-egorov <deathadder98@gmail.com>
2019-03-30 16:39:08 +0300

    Minor improvements

--- js/quote.js
+++ js/quote.js
@@ -13,16 +13,14 @@
 // // newQuoteButton.addEventListener('click', getQuote);
 // getQuote();
 
-const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
-var canvas = document.getElementById("title_canvas"),
-    context = canvas.getContext("2d");
-canvas.height = 1600;
-canvas.width = 2560;
+var endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+var canvas = document.getElementById("title_canvas");
+canvas.setAttribute('width', 1600);
+canvas.setAttribute('height', 1200);
 var w_sample = canvas.width / 3 >> 0;
-var h_sample = canvas.height/2 >> 0;
+var h_sample = canvas.height / 2 >> 0;
 var globalWidth = canvas.width;
 
-var globalHeight = canvas.height;
 
 function getQuote() {
     fetch(endpoint)
@@ -38,41 +36,42 @@ function getQuote() {
 }
 
 function displayQuote(quote) {
-    const quoteText = document.querySelector('.title_canvas');
+    var quoteText = document.querySelector('.title_canvas');
     quoteText.textContent = quote;
 
 }
 
 
-function drawImageScaled(context, img, x,y, fullwidth) {
+function drawImageScaled(x, y, fullwidth) {
     // use context.height and context.width to draw a fullscreen
     // var w_sample = w_sample;
     // if (fullwidth){
     //     w_sample = globalWidth;
     // }
-    var hRatio = w_sample / img.width;
-    var vRatio = h_sample / img.height;
+    var context = document.getElementById("title_canvas").getContext("2d");
+    var hRatio = w_sample / this.width;
+    var vRatio = h_sample / this.height;
     var ratio = Math.min(hRatio, vRatio);
-    var centerShift_x = (w_sample - img.width * ratio) / 2;
-    var centerShift_y = (h_sample - img.height * ratio) / 2;
+    var centerShift_x = (w_sample - this.width * ratio) / 2;
+    var centerShift_y = (h_sample - this.height * ratio) / 2;
     // context.clearRect(x, y, w_sample, h_sample);
-    context.drawImage(img, x, y, img.width, img.height,
-        centerShift_x, centerShift_y, img.width * ratio, img.height * ratio);
+    context.drawImage(this, x, y, this.width, this.height,
+        centerShift_x, centerShift_y, this.width * ratio, this.height * ratio);
+
 }
 
 
-
-function randomUnsplash() {
-
-    img = new Image();
-    img.src = "https://source.unsplash.com/random/";
-    canvas = document.getElementById('title_canvas');
-    canvas.setAttribute('width', window.innerWidth);
-    canvas.setAttribute('height', window.innerHeight);
-    img.onload = function () {
-        drawImageScaled(canvas.getContext("2d"), img)
-    }
-}
+// function randomUnsplash() {
+//
+//     img = new Image();
+//     img.src = "https://source.unsplash.com/random/";
+//     canvas = document.getElementById('title_canvas');
+//     canvas.setAttribute('width', window.innerWidth);
+//     canvas.setAttribute('height', window.innerHeight);
+//     img.onload = function () {
+//         drawImageScaled(canvas.getContext("2d"), img)
+//     }
+// }
 
 /*
 	Collage section setup
@@ -82,47 +81,44 @@ function buildCollage() {
     var images = [];
     var numImages = 6;
     var tempWidth = 0;
-    var src = "https://source.unsplash.com/random/300x200?sig=";
-    var x=0,y=0;
+    var src = "https://source.unsplash.com/random?sig=";
+    var x = 0, y = 0;
     for (var i = 0; i < numImages; i++) {
-        if (tempWidth < globalWidth-1){
-            fetch( src+ Math.random() )
-                .then(function(response ) {
-                return response.blob();
+        if (tempWidth < globalWidth - 1) {
+            fetch(src + Math.random())
+                .then(function (response) {
+                    return response.url;
                 })
                 .then(function (data) {
-                    var base64img= URL.createObjectURL(data);
-                    drawImgFromUnsplash(context,images[i], base64img,x,y,1)
+                    // var base64img= URL.createObjectURL(data);
+                    drawImgFromUnsplash(context, images[i], data, x, y, 0)
                 });
             tempWidth += w_sample;
             x += w_sample;
-        }
-        else {
+        } else {
             tempWidth = 0;
             x = 0;
             y += h_sample;
-            if (i === numImages - 1 ) {
-                fetch( src+ Math.random()
-                ) .then(function(response ) {
-                        return response.blob();
+            if (i === numImages - 1) {
+                fetch(src + Math.random()
+                ).then(function (response) {
+                    return response.blob();
                 }
-                .then(function (data) {
-                    var base64img= URL.createObjectURL(data);
-                    drawImgFromUnsplash(context,images[i], base64img,x,y,1)
-                }));
+                    .then(function (data) {
+                        var base64img = URL.createObjectURL(data);
+                        drawImgFromUnsplash(context, images[i], base64img, x, y, 1)
+                    }));
 
                 // drawImgFromUnsplash(context,images[i],src + Math.random(),x,y,1);
                 break;
             }
-            fetch(  src+ Math.random()
-            ) .then(function(response) {
-                drawImgFromUnsplash(context,images[i], response.url,x,y,0) });
+            fetch(src + Math.random()
+            ).then(function (response) {
+                drawImgFromUnsplash(context, images[i], response.url, x, y, 0)
+            });
             // drawImgFromUnsplash(context,images[i],src + Math.random(),x,y,0);
         }
     }
-
-
-    // renderCollageRows(setup.rows, setup.rowWidths);
 
 }
 
@@ -169,20 +165,16 @@ function scaleWidth(img) {
     return w_sample / img.width;
 }
 
-buildCollage();
 
+function drawImgFromUnsplash(src, x, y, fullwidth) {
+    var image = new Image();
+    // image.addEventListener("load", function() {
+    //     var context = canvas.getContext("2d");
+    //     drawImageScaled(context,this,x,y,fullwidth);
+    // }, false);
+    image.src = src;
+    image.onload = drawImageScaled.bind(image, x, y,fullwidth);
 
-function drawImgFromUnsplash(context, image, src, x, y, fullwidth) {
-    image = new Image();
-    image.src=src;
-    drawImageScaled(context,image,x,y,fullwidth);
-    // image.onload = function () {
-    //     drawImageScaled(context,image,x,y,fullwidth);
-    // };
-    // image.src=src;
-    // image.onload = function () {
-    //     drawImageScaled(context,image,x,y,fullwidth);
-    // };
 
 }
 
@@ -237,6 +229,12 @@ function getLines(context, text, x, y, maxWidth, lineHeight) {
         }
     }
 }
+
+var x = 0, y = 0;
+
+drawImgFromUnsplash("https://source.unsplash.com/400x300?sig=" + Math.random(), x, y, 0);
+x -= w_sample;
+drawImgFromUnsplash("https://source.unsplash.com/400x300?sig=" + Math.random(), x - w_sample, y, 0);
 
 // randomUnsplash();
 
